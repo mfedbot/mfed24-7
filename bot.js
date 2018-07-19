6f8a00500bfa134bf8fbc0d6766f73e4a8160e89
Mantineus <41331723+Mantineus@users.noreply.github.com>
2018-07-20 06:53:52 +0700

    update chao
    CHAO NI MA

--- bot.js
+++ bot.js
@@ -23,5 +23,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'chao') {
+    	message.channel.send('NI MA!');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
