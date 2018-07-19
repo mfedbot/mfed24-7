9a29e9b57dc3d89e7da8d3e1e04264901bcba043
Mantineus <41331723+Mantineus@users.noreply.github.com>
2018-07-19 21:00:28 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,5 +17,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'ching') {
+    	message.channel.send('CHONG!');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
